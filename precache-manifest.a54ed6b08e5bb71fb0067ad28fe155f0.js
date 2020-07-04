67f47187cffa1bf6a59d3012ddfae1d28d8bbefa
codewithmo <mohammedalimomin796@gmail.com>
2020-07-04 22:43:19 +0530

    Updates

---
+++ precache-manifest.a54ed6b08e5bb71fb0067ad28fe155f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c14567803fad58375f2236d983139e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be80ec0c7df129e5c736",
+    "url": "/static/css/main.6e86418f.chunk.css"
+  },
+  {
+    "revision": "5cfa680fefd467540f69",
+    "url": "/static/js/2.ab66f33a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab66f33a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be80ec0c7df129e5c736",
+    "url": "/static/js/main.685ecca7.chunk.js"
+  },
+  {
+    "revision": "92e2dfe38426db94cc6d",
+    "url": "/static/js/runtime-main.536afbf1.js"
+  }
+]);
